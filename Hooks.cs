3522bd3773c542f443896d2fa046e5a08643f900
Some-Bunny <70037543+Some-Bunny@users.noreply.github.com>
2020-12-13 01:22:35 +0000

    Initial commit

---
+++ Hooks.cs
@@ -0,0 +1,32 @@
+﻿using ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using MonoMod.RuntimeDetour;
+using System.Reflection;
+using Dungeonator;
+
+namespace NoFinishedGun
+{
+	// Token: 0x02000018 RID: 24
+	public static class Hooks
+	{
+		// Token: 0x060000B7 RID: 183 RVA: 0x00008CE4 File Offset: 0x00006EE4
+		public static void Init()
+		{
+			try
+			{
+
+				Hook nofinishedgun = new Hook(typeof(RewardManager).GetMethod("ExcludeUnfinishedGunIfNecessary", BindingFlags.Instance | BindingFlags.NonPublic), typeof(FinModule).GetMethod("NoFinishedGun"));
+				//Hook nofinishedgun1 = new Hook(typeof(PickupObject).GetMethod("HandleEncounterable", BindingFlags.Instance| BindingFlags.NonPublic), typeof(FinModule).GetMethod("NoBad"));
+
+			}
+			catch (Exception e)
+			{
+				Tools.PrintException(e, "FF0000");
+			}
+		}
+	}
+}
---
+++ Module.cs
@@ -0,0 +1,49 @@
+﻿using ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using MonoMod.RuntimeDetour;
+using System.Reflection;
+
+namespace NoFinishedGun
+{
+    public class FinModule : ETGModule
+    {
+        public static readonly string MOD_NAME = "No Finished Gun in D Tier";
+        public static readonly string VERSION = "1.0.0";
+        public static readonly string TEXT_COLOR = "#A62919";
+
+        public override void Start()
+        {
+            if (GameStatsManager.Instance.GetFlag(GungeonFlags.ITEMSPECIFIC_AMMONOMICON_COMPLETE))
+            {
+                Gun bastard = PickupObjectDatabase.GetById(83) as Gun;
+                bastard.quality = PickupObject.ItemQuality.EXCLUDED;
+            }
+            else
+            {
+                Log($"Not removed due to lack of Unlock Quota.", TEXT_COLOR);
+            }
+            FakePrefabHooks.Init();
+            //ItemBuilder.Init();
+            //ExamplePassive.Register();
+            Log($"{MOD_NAME} v{VERSION} started successfully.", TEXT_COLOR);
+        }
+
+
+        public PickupObject.ItemQuality quality;
+
+        public static void Log(string text, string color = "#A62919")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+
+        public override void Exit() { }
+        public override void Init()
+        {
+
+        }
+    }
+}
